6ba0d6e1e35085452b48eb0c20444813296056e2
ralfvn <104133171+RalfVanNieuwenhuyse@users.noreply.github.com>
2023-08-09 18:08:55 +0200

    sln seperation in game and engine

---
+++ Qbert/main.cpp
@@ -0,0 +1,42 @@
+#include "pch.h"
+#include <iostream>
+
+#include <SDL.h>
+
+#if _DEBUG
+// ReSharper disable once CppUnusedIncludeDirective
+#if __has_include(<vld.h>)
+#include <vld.h>
+#endif
+#endif
+
+#include "Minigin.h"
+#include "SceneManager.h"
+#include "ResourceManager.h"
+#include "TextObject.h"
+#include "Scene.h"
+
+void load()
+{
+	auto& scene = dae::SceneManager::GetInstance().CreateScene("Demo");
+
+	auto go = std::make_shared<dae::GameObject>();
+	go->SetTexture("background.tga");
+	scene.Add(go);
+
+	go = std::make_shared<dae::GameObject>();
+	go->SetTexture("logo.tga");
+	go->SetPosition(216, 180);
+	scene.Add(go);
+
+	auto font = dae::ResourceManager::GetInstance().LoadFont("Lingua.otf", 36);
+	auto to = std::make_shared<dae::TextObject>("Programming 4 Assignment", font);
+	to->SetPosition(80, 20);
+	scene.Add(to);
+}
+
+int main(int, char* []) {
+	dae::Minigin engine("../Data/");
+	engine.Run(load);
+	return 0;
+}
